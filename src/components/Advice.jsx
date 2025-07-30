bc1664db4a043ec601ee5e4cdc68c4f99dcdb3de
Chukwudi <cchinwenmeri@gmail.com>
2025-07-30 02:06:05 +0100

    bug fixed and design done

--- src/components/Advice.jsx
+++ src/components/Advice.jsx
@@ -1,16 +1,13 @@
+import patterndivider from "../assets/pattern-divider-desktop.svg"
 const Advice = ({advices}) => {
     console.log('this is the component file')
+    console.log(advices.slip)
     return (
-        <div>
-        {
-            advices.map((advice)=>(
-                <div className="advice_slip" key={advice.id}>
-                    <p>{advice.advice}</p>
+                <div className="advice_slip">
+                    <p className="advice_id">ADVICE #{advices.slip.id}</p>
+                    <p className="actualAdvice">"{advices.slip.advice}"</p>
+                    <img src={patterndivider} alt="some cool divider thing" />
                 </div>
-            ))
-        }
-        </div>
-    );
+    );   
 }
- 
 export default Advice;
